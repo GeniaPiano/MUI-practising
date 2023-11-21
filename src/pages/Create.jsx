69650c686d4b36be13492da274bc33af65de1879
GeniaPiano <genia.piano@hotmail.com>
2023-11-21 11:17:47 +0100

    added form for adding tasks

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,48 @@
 
-import {Button, Container, Typography} from "@mui/material";
+import {Button, Container, FormControlLabel, Radio, RadioGroup, Typography, FormLabel, FormControl} from "@mui/material";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {TextField} from "@mui/material";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+
 
 export default function Create() {
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('');
+    const [details, setDetails] = useState('');
+    const [titleError, setTitleError] = useState(false);
+    const [detailsError, setDetailsError] = useState(false);
+    const [category, setCategory] = useState('todos');
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === '') {
+        setTitleError(true);
+    }
+
+    if (details === '') {
+        setDetailsError(true);
+    }
+
+    if (title && details) {
+        fetch('http://localhost:8800/notes', {
+            method: 'POST',
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({title, details, category})
+        }).then(() => navigate('/'))
+        }
+    }
+
+
+
   return (
     <Container>
         <Typography
+            style={{color: 'grey', marginTop: '1rem'}}
             variant="h6"
             color="textSecondary"
             component="h2"
@@ -12,15 +50,54 @@ export default function Create() {
         >
             Create a new note
         </Typography>
-        <Button
-            onClick={() => console.log('You clicked me')}
-            type="submit"
-            color="secondary"
-            variant="contained"
-            //disableElevation
-        >
-            Submit
-        </Button>
+        <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+           <TextField
+           onChange={(e) => setTitle(e.target.value)}
+           style={{marginTop: 40, marginBottom: 20, display: 'block'}}
+           label='Note Title'
+           variant='outlined'
+           color='secondary'
+           required
+           fullWidth
+           error={titleError}
+           />
+
+            <TextField
+                onChange={(e) => setDetails(e.target.value)}
+                style={{marginTop: 40, marginBottom: 20, display: 'block'}}
+                label='Note Title'
+                variant='outlined'
+                color='secondary'
+                multiline
+                rows={4}
+                required
+                fullWidth
+                error={detailsError}
+            />
+            <FormControl> 
+                <FormLabel>Note category</FormLabel>
+                <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+                   <FormControlLabel  value="work" control={<Radio/>} label="work" />
+                   <FormControlLabel  value="todos" control={<Radio/>} label="todos" />
+                   <FormControlLabel  value="reminders" control={<Radio/>} label="reminders" />
+                </RadioGroup>
+            </FormControl>
+
+            <Button
+                style={{
+                    padding: '2rem',
+                    marginTop: '2rem',
+                }}
+                onClick={() => console.log('You clicked me')}
+                type="submit"
+                color="primary"
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon/>}
+            >
+                Submit
+            </Button>
+
+        </form>
 
     </Container>
   )

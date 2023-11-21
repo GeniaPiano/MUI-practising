88ea231cad58c4ea818d8dfe7552c41b5ba986bb
GeniaPiano <genia.piano@hotmail.com>
2023-11-21 11:14:46 +0100

    added ThemeProvider and modified theme for personal usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes.jsx";
 import Create from "./pages/Create.jsx";
+import { ThemeProvider, createTheme} from '@mui/material/styles';
+import {purple} from "@mui/material/colors";
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#e6b3ff',
+        },
+        secondary: purple,
+    },
+    typography: {
+        fontFamily: 'Quicksand',
+        fontWeightLight: 400,
+        fontWeightRegular: 500,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+    },
+});
 
 function App() {
 
-
-  return (
-
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Notes/>}/>
-                <Route path="/create" element={<Create/>} />
-            </Routes>
-        </Router>
-
+      return (
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Notes/>}/>
+                        <Route path="/create" element={<Create/>} />
+                    </Routes>
+                </Router>
+            </ThemeProvider>
   )
 }
 

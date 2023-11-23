da970e21562e0e1970cd97e6b6f9135bb07516f7
GeniaPiano <genia.piano@hotmail.com>
2023-11-23 14:11:53 +0100

    added NoteCard

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,26 @@
+import {Card, CardContent, CardHeader, IconButton, Typography} from "@mui/material";
+import {DeleteOutlined} from "@mui/icons-material";
+
+export default function NoteCard({note, handleDelete}) {
+    return (
+        <div>
+            <Card elevation={3}>
+                <CardHeader
+                    action={
+                    <IconButton onClick={() => handleDelete(note.id)}>
+                        <DeleteOutlined />
+                    </IconButton>
+                    }
+                    title={note.title}
+                    subheader={note.category}
+                />
+
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary">
+                        {note.details}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Container, Grid, Paper} from "@mui/material";
+import NoteCard from "../components/NoteCard.jsx";
 
 export default function Notes() {
-  return (
-    <div>
-      Notes page
-    </div>
-  )
+    const [notes, setNotes] = useState([])
+    const handleDelete = async (id) => {
+        await fetch('http://localhost:8800/notes/' + id, {
+            method: 'DELETE'
+        })
+        const newNotes = notes.filter(note => note.id !== id)
+        setNotes(newNotes);
+    }
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(`http://localhost:8800/notes`);
+                const data = await res.json();
+                await setNotes(data)
+            } catch (err) {
+                console.log(err)
+            }
+        })();
+    }, [])
+
+
+
+
+    return (
+        <Container>
+
+            <Grid container spacing={3}>
+                {notes.map(note => (
+                    <Grid item key={note.id} xs={12} sm={6} md={3}>
+                        <NoteCard note={note} handleDelete={handleDelete}/>
+                    </Grid>
+                ))}
+            </Grid>
+
+        </Container>
+    )
 }
